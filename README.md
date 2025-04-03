535cc589ae6ae997e2fea5536d0c5cb2b609d75b
Jean Paul <jeanpaulmicallef3@gmail.com>
2025-04-03 19:05:21 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jean_paul_micallef_swd62a
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/jean_paul_micallef_swd62a/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jean_paul_micallef_swd62a
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
